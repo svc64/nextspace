585e94edded2ae95b19adc1d89b45ca642886b9e
svc64 <svcx80@icloud.com>
2023-06-26 08:45:37 +0300

    Update versions

--- Packaging/Sources/versions.inc.sh
+++ Packaging/Sources/versions.inc.sh
@@ -1,9 +1,9 @@
 libdispatch_version=5.1.5
-libobjc2_version=2.0
-gnustep_make_version=2.7.0
-gnustep_base_version=1.27.0
-gnustep_gui_version=0.28.0+nextspace
-gnustep_back_version=0.28.0+nextspace
+libobjc2_version=2.1
+gnustep_make_version=2.9.1
+gnustep_base_version=1.29.0
+gnustep_gui_version=0.29.0+nextspace
+gnustep_back_version=0.29.0+nextspace
 
 gorm_version=1.2.26
 projectcenter_version=0.6.2
