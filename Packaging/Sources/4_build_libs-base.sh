c43c6bd241b19d3d1e6199755f9afc76a65cfbcd
svc64 <svcx80@icloud.com>
2023-06-26 08:43:01 +0300

    Add the icu68 patch

--- Packaging/Sources/4_build_libs-base.sh
+++ Packaging/Sources/4_build_libs-base.sh
@@ -4,6 +4,8 @@
 
 . /Developer/Makefiles/GNUstep.sh
 
+git apply extra/icu68.patch --directory nextspace-base-${gnustep_base_version}/Source/
+
 cd ./nextspace-base-${gnustep_base_version} || exit 1
 
 $MAKE_CMD clean
